2694520550065f9d5093275508bcf527e1c50ac3
RM20232930101 <RM20232930101@LAB1-04>
2023-11-14 22:19:48 -0300

    Adicionar arquivos de projeto.

---
+++ MacDonalds/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace MacDonalds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.btnAlterarPedido = new System.Windows.Forms.Button();
+            this.btnExcluirPedido = new System.Windows.Forms.Button();
+            this.btnNovoPedido = new System.Windows.Forms.Button();
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtValor);
+            this.groupBox1.Controls.Add(this.txtQuantidade);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(482, 106);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(31, 13);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantidade:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(46, 42);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(85, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(77, 69);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(54, 20);
+            this.txtQuantidade.TabIndex = 5;
+            // 
+            // btnAlterarPedido
+            // 
+            this.btnAlterarPedido.Location = new System.Drawing.Point(323, 351);
+            this.btnAlterarPedido.Name = "btnAlterarPedido";
+            this.btnAlterarPedido.Size = new System.Drawing.Size(83, 23);
+            this.btnAlterarPedido.TabIndex = 14;
+            this.btnAlterarPedido.Text = "Alterar pedido";
+            this.btnAlterarPedido.UseVisualStyleBackColor = true;
+            this.btnAlterarPedido.Click += new System.EventHandler(this.btnAlterarPedido_Click);
+            // 
+            // btnExcluirPedido
+            // 
+            this.btnExcluirPedido.Location = new System.Drawing.Point(412, 351);
+            this.btnExcluirPedido.Name = "btnExcluirPedido";
+            this.btnExcluirPedido.Size = new System.Drawing.Size(82, 23);
+            this.btnExcluirPedido.TabIndex = 13;
+            this.btnExcluirPedido.Text = "Excluir pedido";
+            this.btnExcluirPedido.UseVisualStyleBackColor = true;
+            this.btnExcluirPedido.Click += new System.EventHandler(this.btnExcluirPedido_Click);
+            // 
+            // btnNovoPedido
+            // 
+            this.btnNovoPedido.Location = new System.Drawing.Point(233, 351);
+            this.btnNovoPedido.Name = "btnNovoPedido";
+            this.btnNovoPedido.Size = new System.Drawing.Size(84, 23);
+            this.btnNovoPedido.TabIndex = 12;
+            this.btnNovoPedido.Text = "Novo pedido";
+            this.btnNovoPedido.UseVisualStyleBackColor = true;
+            this.btnNovoPedido.Click += new System.EventHandler(this.btnNovoPedido_Click);
+            // 
+            // dgvList
+            // 
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Location = new System.Drawing.Point(12, 123);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.Size = new System.Drawing.Size(485, 222);
+            this.dgvList.TabIndex = 11;
+            this.dgvList.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvList_CellMouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 378);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAlterarPedido);
+            this.Controls.Add(this.btnExcluirPedido);
+            this.Controls.Add(this.btnNovoPedido);
+            this.Controls.Add(this.dgvList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Button btnAlterarPedido;
+        private System.Windows.Forms.Button btnExcluirPedido;
+        private System.Windows.Forms.Button btnNovoPedido;
+        private System.Windows.Forms.DataGridView dgvList;
+    }
+}
+
---
+++ MacDonalds/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MacDonalds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregarDadosBanco();
+        }
+
+        private void CarregarDadosBanco()
+        {
+            string conexao = "server=localhost;database=dbMacDonalds;uid=root;pwd=etec";
+            MySqlConnection conexaoMYSQL = new MySqlConnection(conexao);
+            conexaoMYSQL.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("select * from tbPedidoItem", conexaoMYSQL);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgvList.DataSource = dt;
+
+        }
+
+        private void btnAlterarPedido_Click(object sender, EventArgs e)
+        {
+            string conexao = "server=localhost;database=dbMacDonalds;uid=root;pwd=etec";
+            MySqlConnection conexaoMYSQL = new MySqlConnection(conexao);
+            conexaoMYSQL.Open();
+            MySqlCommand comando = new MySqlCommand("update tbPedidoItem set vlProdutoItem=" + txtValor.Text + ", qtde=" + txtQuantidade.Text + " where idPedidoItem=" + txtId.Text, conexaoMYSQL);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Pedido alterado!!!");
+            txtValor.Text = "";
+            txtQuantidade.Text = "";
+            txtId.Text = "";
+            groupBox1.Visible = false;
+            CarregarDadosBanco();
+        }
+
+        private void btnExcluirPedido_Click(object sender, EventArgs e)
+        {
+            DialogResult caixaMensagem = MessageBox.Show("Deseja exluir esse pedido?", "Alerta", MessageBoxButtons.YesNo);
+
+            if (caixaMensagem == DialogResult.Yes)
+            {
+                string conexao = "server=localhost;database=dbMacDonalds;uid=root;pwd=etec";
+                MySqlConnection conexaoMYSQL = new MySqlConnection(conexao);
+                conexaoMYSQL.Open();
+                MySqlCommand comando = new MySqlCommand("delete from tbPedidoItem where idPedidoItem=" + txtId.Text + ";", conexaoMYSQL);
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Pedido excluído com sucesso!");
+                txtValor.Text = "";
+                txtQuantidade.Text = "";
+                txtId.Text = "";
+                groupBox1.Visible = false;
+                CarregarDadosBanco();
+            }
+        }
+
+        private void dgvList_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            txtId.Text = dgvList.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtValor.Text = dgvList.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtQuantidade.Text = dgvList.Rows[e.RowIndex].Cells[2].Value.ToString();
+            groupBox1.Visible = true;
+        }
+
+        private void btnNovoPedido_Click(object sender, EventArgs e)
+        {
+            string conexao = "server=localhost;database=dbMacDonalds;uid=root;pwd=etec";
+            MySqlConnection conexaoMYSQL = new MySqlConnection(conexao);
+            conexaoMYSQL.Open();
+            MySqlCommand comando = new MySqlCommand("insert into tbPedidoItem (vlProdutoItem,qtde) values (" + txtValor.Text + "," + txtQuantidade.Text + ");", conexaoMYSQL);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Novo pedido realizado com sucesso!!!");
+            txtValor.Text = "";
+            txtQuantidade.Text = "";
+            txtId.Text = "";
+            groupBox1.Visible = false;
+            CarregarDadosBanco();
+        }
+    }
+}
